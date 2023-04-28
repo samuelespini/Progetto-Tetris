1288803b5a476d16d853d721d6cea4b62036ef75
samuelespini <samuele.spini@mpolo.it>
2023-04-28 09:29:22 +0200

    modfica prova
    ciao

--- src/tetrisgui/GUITetris.java
+++ src/tetrisgui/GUITetris.java
@@ -12,7 +12,10 @@ public class GUITetris extends javax.swing.JFrame {
         initComponents();
         setLocationRelativeTo(null);
         tab = new TabellaGioco(this);
-
+        a
+                a
+                a
+                        
     }
 
     /**
